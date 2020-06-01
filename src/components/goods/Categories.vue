92b903cfc9ee8abb59b87bc70b0a5b657f40991d
Blanckant <2805858091@qq.com>
2020-06-01 15:16:39 +0800

    提交

--- src/components/goods/Categories.vue
+++ src/components/goods/Categories.vue
@@ -3,8 +3,8 @@
     <!-- 面包屑导航区域 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
     </el-breadcrumb>
 
     <el-card>
